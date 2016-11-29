dbd87a4f19e55d62aae0c1a08e3d69a6ae61c648
andela-sgaamuwa <samuel.gaamuwa@andela.com>
2016-11-29 09:54:26 +0300

    [#135013467](Feature) Owner Permissions Adds owner permissions for items as well

--- api/permissions.py
+++ api/permissions.py
@@ -1,12 +1,16 @@
-from rest_framework.permissions import BasePermission
+from api.models import Bucketlist, Item
+from rest_framework import permissions
 
 
-class IsOwner(BasePermission):
+class IsOwner(permissions.BasePermission):
     """
     Object-level permission to only allow only owners to
     Interact with an object, that is view, edit and create
     """
 
-    def has_object_permissions(self, request, view, obj):
+    def has_object_permission(self, request, view, obj):
         # return true if user is the owner of bucketlist
-        return obj.created_by == request.user
+        if type(obj) is Bucketlist:
+            return obj.created_by == request.user
+        elif type(obj) is Item:
+            return obj.bucketlist.created_by == request.user
