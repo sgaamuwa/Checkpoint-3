f2e830c22d71afe2d77f0c40a3c84d02489fb6c0
andela-sgaamuwa <samuel.gaamuwa@andela.com>
2016-11-29 11:59:12 +0300

    [settings.py](Chore) SECRET KEY Removes and changes the secret key

--- ebyokola/settings.py
+++ ebyokola/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2l#m$ywvq-ut+br0e04i1#lhxq@09#8he2^2w!8i3w9&h5d&1d'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
