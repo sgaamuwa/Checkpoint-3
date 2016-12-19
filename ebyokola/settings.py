1faec7296683da4558d2dc29fe1820eb3340a79a
andela-sgaamuwa <samuel.gaamuwa@andela.com>
2016-12-19 17:01:43 +0300

    (Chore) Adds default database for Heroku

--- ebyokola/settings.py
+++ ebyokola/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 import sys
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +27,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ebyokola.herokuapp.com", "localhost", "0.0.0.0"]
 
 
 # Application definition
@@ -117,6 +118,8 @@ if 'test' in sys.argv:
             'NAME': 'mydatabase'
         }
     }
+elif 'DATABASE_URL' in os.environ:
+    DATABASES = {'default': dj_database_url.config()}
 else:
     DATABASES = {
         'default': {
